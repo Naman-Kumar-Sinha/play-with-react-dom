53192a5969ce9ba47dfb261be6d9bd8e29e4b2e8
NKS <niranjan.sinha@applylab.io>
2018-02-10 17:57:53 +0530

    Comments and document

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,17 @@
+/**
+ * App Component
+ * ./src/components/App.jsx file
+ */
+
 import React from 'react'
 
+/**
+ * 
+ * Default class for App component
+ * @export
+ * @class App
+ * @extends {React.Component}
+ */
 export default class extends React.Component {
     render() {
         return (
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+/**
+ * ./src/index.js file
+ */
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App.jsx'
